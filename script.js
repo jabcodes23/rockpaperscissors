317c91908508717644caea871a5942649984ad64
jabcodes23 <jabwrites23@gmail.com>
2020-12-22 15:00:05 -0500

    Added UI

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let playerScore = 0;
+let cpuScore = 0;
+
+// console.log(game());
+
+function game() {
+    while(playerScore + cpuScore < 5) {
+        console.log(playRound(prompt("Rock, paper, or scissors?", ""), computerPlay()));
+    }
+
+    let winner = (playerScore > cpuScore) ? "Player" : "Computer";
+    return `The final score is Player: ${playerScore}, Computer: ${cpuScore}....so the winner is ${winner}!`;
+}
+
 function computerPlay () {
     const objectThrown = ["Rock", "Paper", "Scissors"];
     const random = Math.floor(Math.random() * objectThrown.length);
@@ -11,13 +25,16 @@ function playRound (player, cpu) {
     if ((player == "rock" && cpu == "paper") || 
         (player == "paper" && cpu == "scissors") || 
         (player == "scissors" && cpu == "rock")) {
-        return `Sorry, you lose, ${cpu} beats ${player}.`;
+        cpuScore++;
+        return `Sorry, you lose, ${cpu} beats ${player}. Score: Player - ${playerScore}, Computer - ${cpuScore}`;
     } else if (player == cpu) {
-        return `Both players threw ${player}. Try again.`;
+        return `Both players threw ${player}. Trying again...`;
     } else {
-        return `You win, ${player} beats ${cpu}!`;
+        playerScore++;
+        return `You win, ${player} beats ${cpu}! Score: Player - ${playerScore}, Computer - ${cpuScore}`;
     }
 }
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+
+
+
+
