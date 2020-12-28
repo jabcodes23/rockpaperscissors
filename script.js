ef74b2c88c1e03df52a5061d1f58d5efabc82d04
jabcodes23 <jabwrites23@gmail.com>
2020-12-28 11:36:03 -0500

    Added some button functionality

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 let playerScore = 0;
 let cpuScore = 0;
 
+let rockImg = 'https://images.homedepot-static.com/productImages/23d91e85-aa47-4200-91d5-5fe314e0da98/svn/backyard-x-scapes-fake-rocks-knt-rb1-c3_1000.jpg';
+let paperImg = 'https://media.istockphoto.com/vectors/crumpled-paper-texture-white-battered-paper-background-white-empty-vector-id960504202?k=6&m=960504202&s=612x612&w=0&h=Zjt5u788AtgEPWDVct5pe5DKSxFPzJtAzKmP5DATa1Q=';
+let scissorsImg = 'https://www.ikea.com/us/en/images/products/sy-scissors__0112301_PE263788_S5.JPG?f=sg';
+
 // console.log(game());
+
+document.getElementById("rock").onclick = function() {
+  console.log('rock tapped')
+  document.getElementById("player_piece").src = 'https://images.homedepot-static.com/productImages/23d91e85-aa47-4200-91d5-5fe314e0da98/svn/backyard-x-scapes-fake-rocks-knt-rb1-c3_1000.jpg';
+};
+
+
+function throwPaper() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
+
+function throwScissors() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
 
 function game() {
     while(playerScore + cpuScore < 5) {
